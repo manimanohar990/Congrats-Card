653b7c2e797d87b8eef8596a5fbf8b76266d2fbc
root <manoharmani990@gmail.com>
2023-09-21 11:18:30 +0000

    Congrats Card

---
+++ index.js
@@ -0,0 +1,25 @@
+const element = (
+  <div class="container">
+    <h1 class="main-head">Congratulations</h1>
+    <div class="card-container">
+      <div class="img-con">
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png"
+          class="icon"
+        />
+      </div>
+      <h1 class="head">Kiran V</h1>
+      <p class="para">
+        Vishnu institute of computer Education and Technology, Bhimavaram
+      </p>
+      <div class="img-con">
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png"
+          class="icon"
+        />
+      </div>
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
